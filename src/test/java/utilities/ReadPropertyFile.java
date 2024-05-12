71608fd35b61dab1d8b967854d2742e97e911511
_nafiz <nafiz.hossain@tigerit.com>
2024-05-12 14:18:59 +0600

    feat: Add utility class for reading configuration properties

--- src/test/java/utilities/ReadPropertyFile.java
+++ src/test/java/utilities/ReadPropertyFile.java
@@ -1,10 +1,17 @@
 package utilities;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
 public class ReadPropertyFile {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws IOException{
+		FileReader fr = new FileReader("/home/nafiz/TigerIT/projects/Personal/SeleniumCraft/src/test/resources/configfiles/config.properties");
+		Properties p = new Properties();
+		p.load(fr);
+		System.out.println(p.getProperty("browser"));
 	}
 
 }
